b5786d4eaf9ac1aa648c80ced69afea78d9f7cad
Fawad <nizamdeen.20250246@iit.ac.lk>
2026-07-27 14:32:35 +0530

    add a code in file manager class and add some code in hello  application class

--- src/main/java/com/example/malabetuktuk/FileManager.java
+++ src/main/java/com/example/malabetuktuk/FileManager.java
@@ -1,4 +1,34 @@
 package com.example.malabetuktuk;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class FileManager {
+
+    public static void readInventoryFile() {
+
+        try {
+
+            BufferedReader reader = new BufferedReader(
+                    new FileReader("src/main/resources/data/inventory_legacy.txt"));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                System.out.println(line);
+
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Error reading inventory file.");
+            e.printStackTrace();
+
+        }
+
+    }
 }
